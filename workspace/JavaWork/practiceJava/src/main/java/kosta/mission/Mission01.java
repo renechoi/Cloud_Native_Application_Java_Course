90c8ff8c62b44d9ab5b29baea3d935e24de00e7b
renechoi <renechoi90@gmail.com>
2022-12-21 12:23:54 +0900

    feat(day2) : variable practice - print practice

---
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/mission/Mission01.java
@@ -0,0 +1,14 @@
+package kosta.mission;
+
+public class Mission01 {
+
+    public static void main(String[] args) {
+        // TODO : 이름, 나이, 주소를 변수에 초기화 한 후 콘솔에 출력하자.
+
+        String name = "홍길동";
+        int age = 20;
+        String address = "서울시 자바구 스프링로 2022-12";
+
+        System.out.printf("이름 :%s 나이 :%d 주소 :%s", name, age, address);
+    }
+}
