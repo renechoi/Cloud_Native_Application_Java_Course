4bb35ed38a33ad4288a4b97f5cb2dcd79bca8c60
renechoi <renechoi90@gmail.com>
2022-12-21 16:57:39 +0900

    feat(day2) : practice getting input with scanner method

--- workspace/JavaWork/practiceJava/src/main/java/kosta/mission/Mission02.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/mission/Mission02.java
@@ -2,6 +2,7 @@ package kosta.mission;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Mission02 {
     public static void main(String[] args) {
@@ -9,9 +10,9 @@ public class Mission02 {
         // 국어, 영어, 수학 성적을 입력받은 후
         // 총점과 평균을 출력해 보자.
 
-        int korean= 80;
-        int english= 90;
-        int math= 95;
+        int korean = 80;
+        int english = 90;
+        int math = 95;
 
         int scoreTotal = korean + english + math;
         int scoreAverage = scoreTotal / 3;
@@ -22,7 +23,6 @@ public class Mission02 {
         System.out.println("총점: " + scoreTotal);
         System.out.println("평균: " + scoreAverage2);
         System.out.println("평균: " + scoreAverage3);
-
 
 
 //        int testCasting = (int) 3.14;
@@ -45,7 +45,53 @@ public class Mission02 {
         System.out.println(profitAmount);
 
 
+        // int <==> char (아스키 코드 값)
 
+        for (int i = 65; i <= 90; i++) {
+            System.out.print((char) i);
+        }
+
+        System.out.println();
+
+        // 정수형 <==> String
+
+        String numberString = "2000";
+        int numberInt = Integer.parseInt(numberString);
+        System.out.println(numberInt + 1000);
+
+        String numberString2 = "2000한글";
+        try {
+            int numberInt2 = Integer.parseInt(numberString2);
+            System.out.println(numberInt2 + 1000);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+
+
+        int number10 = 100;
+        String number11 = String.valueOf(number10);
+        String number12 = number10 + "";
+        System.out.println(number12);
+
+        //퀴즈
+        String string2 = 7 + "7" + 7; // 777
+        System.out.println(string2);
+
+
+
+        // 입력 받기
+
+        Scanner scanner = new Scanner(System.in);
+//        scanner.nextInt();  // 숫자로 입력 받을 때
+//        scanner.nextLine(); // 문자로 입력 받을 때
+
+        System.out.println("x: ");
+        int x = scanner.nextInt();
+
+        System.out.println("y: ");
+        int y = scanner.nextInt();
+
+        System.out.printf("result : %d", x*y);
 
 
     }
@@ -69,8 +115,8 @@ public class Mission02 {
         }
     }
 
-    public class inputView{
-        public int getInput(){
+    public class inputView {
+        public int getInput() {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             return 0;
         }
