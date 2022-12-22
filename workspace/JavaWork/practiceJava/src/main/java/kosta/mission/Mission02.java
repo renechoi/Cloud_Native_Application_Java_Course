1fcb1a0f2078df7353aa8591eed2442f5265b8fb
renechoi <renechoi90@gmail.com>
2022-12-22 15:44:29 +0900

    feat(day3) : practice grading program with if-else statement

--- workspace/JavaWork/practiceJava/src/main/java/kosta/mission/Mission02.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/mission/Mission02.java
@@ -7,6 +7,33 @@ import java.util.Scanner;
 
 public class Mission02 {
     public static void main(String[] args) {
+
+    }
+
+    private static void gradingProgramWithIfAndElse(){
+        int korean = 80;
+        int english = 90;
+        int math = 95;
+
+        System.out.println(calculation(korean));
+
+    }
+
+    private static String calculation(int score){
+        if (score >=90){
+            return "A";
+        } else if (score >= 80){
+            return "B";
+        } else if (score >=70){
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    private static void gradingProgram() {
         // TODO 성적관리 프로그램을 구현하자
         // 국어, 영어, 수학 성적을 입력받은 후
         // 총점과 평균을 출력해 보자.
@@ -26,7 +53,37 @@ public class Mission02 {
         System.out.println("평균: " + scoreAverage3);
 
 
-//        int testCasting = (int) 3.14;
+        // 입력 받기
+
+        Scanner scanner = new Scanner(System.in);
+//        scanner.nextInt();  // 숫자로 입력 받을 때
+//        scanner.nextLine(); // 문자로 입력 받을 때
+//
+//        System.out.println("x: ");
+//        int x = scanner.nextInt();
+//
+//        System.out.println("y: ");
+//        int y = scanner.nextInt();
+
+//        System.out.printf("result : %d", x*y);
+
+        System.out.println("국어 점수를 입력해주세요:");
+        korean = scanner.nextInt();
+        System.out.println("영어 점수를 입력해주세요:");
+        english = scanner.nextInt();
+        System.out.println("수학 점수를 입력해주세요:");
+        math = scanner.nextInt();
+
+        scoreTotal = korean + english + math;
+        scoreAverage2 = scoreTotal / 3.0;
+
+        System.out.println("총점: " + scoreTotal);
+        System.out.println("평균: " + scoreAverage2);
+    }
+
+    private static void practiceCasting() {
+
+        //        int testCasting = (int) 3.14;
 
 
 //        double testFloatCalculation = 3.14 + 1;
@@ -77,38 +134,7 @@ public class Mission02 {
         //퀴즈
         String string2 = 7 + "7" + 7; // 777
         System.out.println(string2);
-
-
-
-        // 입력 받기
-
-        Scanner scanner = new Scanner(System.in);
-//        scanner.nextInt();  // 숫자로 입력 받을 때
-//        scanner.nextLine(); // 문자로 입력 받을 때
-//
-//        System.out.println("x: ");
-//        int x = scanner.nextInt();
-//
-//        System.out.println("y: ");
-//        int y = scanner.nextInt();
-
-//        System.out.printf("result : %d", x*y);
-
-        System.out.println("국어 점수를 입력해주세요:");
-        korean = scanner.nextInt();
-        System.out.println("영어 점수를 입력해주세요:");
-        english = scanner.nextInt();
-        System.out.println("수학 점수를 입력해주세요:");
-        math = scanner.nextInt();
-
-        scoreTotal = korean + english + math;
-        scoreAverage2 = scoreTotal / 3.0;
-
-        System.out.println("총점: " + scoreTotal);
-        System.out.println("평균: " + scoreAverage2);
-
     }
-
 
 
     private static void implicitCastingExample(){
