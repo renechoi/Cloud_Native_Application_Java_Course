3f8b7f2ee9718d00d644fd47f666ec66ef7e3863
renechoi <renechoi90@gmail.com>
2022-12-28 12:13:21 +0900

    feat(day7) : practice handling exception

---
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/basic/day007/ExceptionExam.java
@@ -0,0 +1,46 @@
+package kosta.basic.day007;
+
+public class ExceptionExam {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static void handleThrownMethod() {
+        try{
+            practiceThrowMethod();
+        } catch (Exception e){
+            e.printStackTrace();    // 예외 발생한 자취를 추적해준다.
+        }
+    }
+
+    private static void practiceThrowMethod() throws Exception {
+        int a = 10;
+        int b = 10;
+
+        if (a==b){
+         throw new Exception("같으면 안돼");
+        }
+    }
+
+    private static void practiceFinallyStatement() {
+        int a = 10;
+        int b = 0;
+
+        try {
+            int divided = a / b;
+            System.out.println(divided);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("4");
+        }
+    }
+
+    private static void exceptionEx1() {
+        int a = 10;
+        int b = 0;
+        System.out.println(a / b);
+    }
+}
