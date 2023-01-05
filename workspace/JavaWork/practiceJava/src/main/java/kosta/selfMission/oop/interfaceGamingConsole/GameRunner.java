156876cc13f3cd4259a73707615558d0e7d73247
renechoi <renechoi90@gmail.com>
2023-01-06 00:17:11 +0900

    feat(day13): exercise polymorphism with interface

--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/oop/interfaceGamingConsole/GameRunner.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/oop/interfaceGamingConsole/GameRunner.java
@@ -2,10 +2,25 @@ package kosta.selfMission.oop.interfaceGamingConsole;
 
 public class GameRunner {
     public static void main(String[] args) {
+
         MarioGame marioGame = new MarioGame();
         marioGame.up();
 
         ChessGame chessGame = new ChessGame();
         chessGame.left();
+
+        InterfaceGamingConsole[] games = {new MarioGame(), new ChessGame()};
+
+        for (InterfaceGamingConsole game : games){
+            game.up();
+            game.down();
+            game.left();
+            game.right();
+        }
+
+        // 같은 코드를 다른 두 클래스에서 호출하면서 다른 구현을 시킴 => 다형성
+
+
+
     }
 }
