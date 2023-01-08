b0b29a583ca07cbb547a7de1853ec54dc09421bb
renechoi <renechoi90@gmail.com>
2023-01-09 00:13:57 +0900

    feat(til): generics implementation

--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/generics/GenericsMain.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/generics/GenericsMain.java
@@ -1,18 +1,28 @@
 package kosta.selfMission.others.generics;
 
-import java.util.ArrayList;
-
 public class GenericsMain {
     public static void main(String[] args) {
 
-
+        withGenericsProblemSolved();
 
     }
 
-    private static void whyGenericsIsNeeded() {
-        MyCustomList list = new MyCustomList();
+    private static void withGenericsProblemSolved() {
+        MyCustomList<String> list = new MyCustomList<>();
         list.addElement("el 1");
         list.addElement("el 2");
+        String value = list.get(0);
+
+        MyCustomList<Integer> list2 = new MyCustomList<>();
+        list2.addElement(1);
+        list2.addElement(2);
+        Integer number = list2.get(0);
+    }
+
+    private static void whyGenericsIsNeeded() {
+//        MyCustomList list = new MyCustomList();
+//        list.addElement("el 1");
+//        list.addElement("el 2");
 
         // 만약에 여기서 기능이 정확하게 일치하는데 String이 아니라 integer 클래스를 만들고 싶다면 ?
         // => generics이 필요한 이유
--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/generics/MyCustomList.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/generics/MyCustomList.java
@@ -2,14 +2,18 @@ package kosta.selfMission.others.generics;
 
 import java.util.ArrayList;
 
-public class MyCustomList {
-    ArrayList<String> list = new ArrayList<>();
+public class MyCustomList<T> {
+    ArrayList<T> list = new ArrayList<>();
 
-    public void addElement(String element){
+    public void addElement(T element){
         list.add(element);
     }
 
-    public void removeElement(String element){
+    public void removeElement(T element){
         list.remove(element);
+    }
+
+    public T get(int index) {
+        return list.get(index);
     }
 }
