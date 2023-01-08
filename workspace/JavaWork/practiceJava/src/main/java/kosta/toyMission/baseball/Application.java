670869453f0111a662e0099dd0f84f066d92f8ae
renechoi <renechoi90@gmail.com>
2023-01-08 09:13:49 +0900

    feat(baseball Tpj): handle runtime exception

--- workspace/JavaWork/practiceJava/src/main/java/kosta/toyMission/baseball/Application.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/toyMission/baseball/Application.java
@@ -1,8 +1,15 @@
 package kosta.toyMission.baseball;
 
+import kosta.toyMission.baseball.controller.GameController;
+import kosta.toyMission.baseball.ui.outputView.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        GameController controller = new GameController();
-        controller.run();
+        try {
+            GameController controller = new GameController();
+            controller.run();
+        } catch (RuntimeException e){
+            OutputView.printUnhandledExceptionMessage("예기치 못한 오류가 발생했어요. 프로그램을 종료합니다.");
+        }
     }
 }
