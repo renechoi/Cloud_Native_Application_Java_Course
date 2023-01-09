9f511cfcaadba88ed644e5ab2f2e4ba3ad99aabf
renechoi <renechoi90@gmail.com>
2023-01-09 23:53:06 +0900

    feat(self): practice stream - for each

--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/Main.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/Main.java
@@ -13,6 +13,11 @@ public class Main {
     }
 
     private static void printWithFp(List<String> lists) {
+        // 함수형 프로그래밍의 특징은 함수를 일급 시티즌으로 대우한다는 것이다
+        // stream에서 for each로 element를 돌면서 데이터를 보내는 것이 아니라 함수, 즉 action을 보낸다
+        // function을 sending 한다 !
+
+
         lists.stream().forEach(System.out::println);
         lists.stream().forEach(element -> System.out.println(element));
     }
