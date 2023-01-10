ba3ef0d5d85b36da0dbe28545131c00c18af2c73
renechoi <renechoi90@gmail.com>
2023-01-10 22:49:22 +0900

    feat(self): practice functional programming with - implement custom consumer

--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/PredicateExample.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/PredicateExample.java
@@ -1,6 +1,7 @@
 package kosta.selfMission.others.functionalProgramming;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class PredicateExample {
@@ -8,6 +9,35 @@ public class PredicateExample {
 
 
     public static void main(String[] args) {
+
+        List.of(23, 43, 34, 45, 36, 48)
+                .stream()
+                .filter(new EvenNumberPredicate())
+                .forEach(new SystemOutConsumer());
+
+        // foreach => consumer 인터페이스를 구혀
+//        @FunctionalInterface
+//        public interface Consumer<T> {
+//
+//            /**
+//             * Performs this operation on the given argument.
+//             *
+//             * @param t the input argument
+//             */
+//            void accept(T t);
+
+
+
+    }
+
+    private static void predicateCustom() {
+        List.of(23, 43, 34, 45)
+                .stream()
+                .filter(new EvenNumberPredicate())
+                .forEach(e-> System.out.println(e));
+    }
+
+    private static void predicateOriginal() {
         List.of(23, 43, 34, 45)
                 .stream()
                 .filter(n -> n%2 ==0)
@@ -26,13 +56,6 @@ public class PredicateExample {
         //     * otherwise {@code false}
         //     */
         //    boolean test(T t);
-
-
-
-        List.of(23, 43, 34, 45)
-                .stream()
-                .filter(new EvenNumberPredicate())
-                .forEach(e-> System.out.println(e));
     }
 }
 
@@ -42,5 +65,13 @@ class EvenNumberPredicate implements Predicate<Integer>{
     @Override
     public boolean test(Integer number) {
         return number % 2 ==0;
+    }
+}
+
+class SystemOutConsumer implements Consumer<Integer>{
+
+    @Override
+    public void accept(Integer number) {
+        System.out.println(number);
     }
 }
