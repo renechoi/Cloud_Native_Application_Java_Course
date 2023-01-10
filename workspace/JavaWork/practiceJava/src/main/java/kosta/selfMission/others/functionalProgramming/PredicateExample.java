7637f758d5126cdcb69702da75060e39332b9029
renechoi <renechoi90@gmail.com>
2023-01-10 22:54:42 +0900

    feat(self): practice functional programming with - implement custom map

--- workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/PredicateExample.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/selfMission/others/functionalProgramming/PredicateExample.java
@@ -2,14 +2,41 @@ package kosta.selfMission.others.functionalProgramming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class PredicateExample {
 
 
-
     public static void main(String[] args) {
 
+        List.of(23, 43, 34, 45, 36, 48)
+                .stream()
+                .filter(new EvenNumberPredicate())
+                .map(new NumberSquareMapper())
+                .forEach(new SystemOutConsumer());
+
+
+        //    <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+        // map -> Function 인터페이스 => 인자를 받아서 다른 값으로 리턴한다
+
+//        @FunctionalInterface
+//        public interface Function<T, R> {
+//
+//            /**
+//             * Applies this function to the given argument.
+//             *
+//             * @param t the function argument
+//             * @return the function result
+//             */
+//            R apply(T t);
+//
+
+
+    }
+
+    private static void practiceConsumer() {
         List.of(23, 43, 34, 45, 36, 48)
                 .stream()
                 .filter(new EvenNumberPredicate())
@@ -25,23 +52,20 @@ public class PredicateExample {
 //             * @param t the input argument
 //             */
 //            void accept(T t);
-
-
-
     }
 
     private static void predicateCustom() {
         List.of(23, 43, 34, 45)
                 .stream()
                 .filter(new EvenNumberPredicate())
-                .forEach(e-> System.out.println(e));
+                .forEach(e -> System.out.println(e));
     }
 
     private static void predicateOriginal() {
         List.of(23, 43, 34, 45)
                 .stream()
-                .filter(n -> n%2 ==0)
-                .forEach(e-> System.out.println(e));
+                .filter(n -> n % 2 == 0)
+                .forEach(e -> System.out.println(e));
 
         // filter :    Stream<T> filter(Predicate<? super T> predicate);
 
@@ -60,18 +84,26 @@ public class PredicateExample {
 }
 
 
-class EvenNumberPredicate implements Predicate<Integer>{
+class EvenNumberPredicate implements Predicate<Integer> {
 
     @Override
     public boolean test(Integer number) {
-        return number % 2 ==0;
+        return number % 2 == 0;
     }
 }
 
-class SystemOutConsumer implements Consumer<Integer>{
+class SystemOutConsumer implements Consumer<Integer> {
 
     @Override
     public void accept(Integer number) {
         System.out.println(number);
+    }
+}
+
+class NumberSquareMapper implements Function<Integer, Integer> {
+
+    @Override
+    public Integer apply(Integer number) {
+        return number * number;
     }
 }
