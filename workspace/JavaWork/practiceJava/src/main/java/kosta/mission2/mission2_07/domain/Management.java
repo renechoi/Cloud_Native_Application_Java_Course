6da4ad82f2569a233d0470ccd2e2a64e34d9e931
renechoi <renechoi90@gmail.com>
2023-01-12 21:03:21 +0900

    feat(day 18): implement file save, load function

--- workspace/JavaWork/practiceJava/src/main/java/kosta/mission2/mission2_07/domain/Management.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/mission2/mission2_07/domain/Management.java
@@ -1,16 +1,17 @@
 package kosta.mission2.mission2_07.domain;
 
-import kosta.mission2.mission2_07.domain.command.CommandSort;
+import kosta.mission2.mission2_07.domain.command.SortCommand;
 import kosta.mission2.mission2_07.domain.contact.ContactGeneral;
 import kosta.mission2.mission2_07.domain.contact.ContactUniversity;
 import kosta.mission2.mission2_07.ui.InputView;
 
+import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Management {
+public class Management implements Serializable {
 
     public static final String ADDITION = "추가할";
     public static final String SEARCH = "검색할";
@@ -20,90 +21,148 @@ public class Management {
 
     List<ContactGeneral> contacts = new ArrayList<>();
 
-    public void addInfo() {
-
-        String type = InputView.getType();
-        String name = InputView.getName(ADDITION);
-        String number = InputView.getNumber();
-        LocalDate localDate = LocalDate.of(InputView.getDobYear(), InputView.getDobMonth(), InputView.getDobDate());
-
-        switch (type) {
-            case "일반" -> contacts.add(new ContactGeneral(type, name, number, localDate));
-            case "동창" -> contacts.add(new ContactUniversity(type, name, number, localDate));
+    public Result addInfo() {
+        try {
+            String type = InputView.getType();
+            String name = InputView.getName(ADDITION);
+            String number = InputView.getNumber();
+            LocalDate localDate = LocalDate.of(InputView.getDobYear(), InputView.getDobMonth(), InputView.getDobDate());
+            switch (type) {
+                case "일반" -> contacts.add(new ContactGeneral(type, name, number, localDate));
+                case "동창" -> contacts.add(new ContactUniversity(type, name, number, localDate));
+            }
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
         }
+        return new Result(true, false);
     }
 
-    public void printInfoAll() {
-        if (contacts.isEmpty()) {
-            System.out.println("contacts is empty!");
-            return;
+    public Result printInfoAll() {
+        try {
+            if (contacts.isEmpty()) {
+                System.out.println("contacts is empty!");
+                return new Result(true, false);
+            }
+            for (ContactGeneral contact : contacts) {
+                contact.printContactInfo();
+            }
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
         }
-        for (ContactGeneral contact : contacts) {
-            contact.printContactInfo();
+        return new Result(true, false);
+    }
+
+    public Result printInfoByName() {
+        try {
+            String nameRequested = InputView.getName(SEARCH);
+            getContact(nameRequested).printContactInfo();
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
         }
+        return new Result(true, false);
     }
 
-    public void printInfoByName() {
-        String nameRequested = InputView.getName(SEARCH);
-        getContact(nameRequested).printContactInfo();
+    public Result update() {
+        try {
+            String nameFrom = InputView.getName(UPDATE_FROM);
+            String nameTo = InputView.getName(UPDATE_TO);
+            getContact(nameFrom).setName(nameTo);
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void update() {
-        String nameFrom = InputView.getName(UPDATE_FROM);
-        String nameTo = InputView.getName(UPDATE_TO);
-
-        getContact(nameFrom).setName(nameTo);
+    public Result delete() {
+        try {
+            String nameToBeDeleted = InputView.getName(DELETE);
+            contacts.remove(getContact(nameToBeDeleted));
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void delete() {
-        String nameToBeDeleted = InputView.getName(DELETE);
-        contacts.remove(getContact(nameToBeDeleted));
+    public Result handleSortRequest(Management management) {
+        try {
+            SortCommand command = SortCommand.of(InputView.getSortCommand());
+            command.handleCommand(management);
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.CRUD_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void handleSortRequest(Management management) {
-        CommandSort command = CommandSort.of(InputView.getSortCommand());
-        command.sortAction(management);
+    // TODO : sort 작업만 별도로 수행하는 (내부) 클래스 구현 고려 !
+    public Result sortByType() {
+        try {
+            contacts.sort(Comparator.comparing(ContactGeneral::getType));
+        } catch(RuntimeException e){
+        throw new NotSupportOperationException(NotSupportOperationException.SORT_NOT_SUPPORT_OPERATION_EXCEPTION);
+
+        }
+        return new Result(true, false);
     }
 
-    public void sortByType() {
-        contacts.sort(Comparator.comparing(ContactGeneral::getType));
+    public Result sortByName() {
+        try {
+            contacts.sort(Comparator.comparing(ContactGeneral::getName));
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.SORT_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void sortByName() {
-        contacts.sort(Comparator.comparing(ContactGeneral::getName));
+    public Result sortByNumber() {
+        try {
+            contacts.sort(Comparator.comparing(ContactGeneral::getNumber));
+        } catch (RuntimeException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.SORT_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void sortByNumber() {
-        contacts.sort(Comparator.comparing(ContactGeneral::getNumber));
+    public Result sortByDob() {
+        try {
+            contacts.sort(Comparator.comparing(ContactGeneral::getDob));
+        } catch (RuntimeException e){
+            throw new NotSupportOperationException(NotSupportOperationException.SORT_NOT_SUPPORT_OPERATION_EXCEPTION);
+        }
+        return new Result(true, false);
     }
 
-    public void sortByDob() {
-        contacts.sort(Comparator.comparing(ContactGeneral::getDob));
+
+    public Result saveFile() {
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("contactInfoVer1.btc"));
+            objectOutputStream.writeObject(contacts);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new NotSupportOperationException(NotSupportOperationException.FILE_SAVING_IO_EXCEPTION);
+        }
+        return new Result(true, false);
+    }
+
+    public Result loadFile() {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("contactInfoVer1.btc"));
+            this.contacts = (List<ContactGeneral>) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new NotSupportOperationException(NotSupportOperationException.FILE_LOADING_IO_EXCEPTION);
+        }
+        return new Result(true, false);
+    }
+
+    public Result quit() {
+        System.out.println("quit!");
+        return new Result(false, true);
     }
 
     private ContactGeneral getContact(String nameFrom) {
         return contacts.stream()
-                .filter(c -> c.getName().equals(nameFrom))
+                .filter(contact -> contact.getName().equals(nameFrom))
                 .findFirst()
                 .orElseThrow(() -> new InvalidContactException(InvalidContactException.CONTACT_NOT_FOUND));
     }
 }
-
-// Sort Action에 대해 별도의 클래스를 만들어 관리하는 것에 대해 고민해보았다. 결과는... 놉.. ㅠㅠ
-//class SortAction{
-//    private void sortByType(List<ContactGeneral> contacts) {
-//        contacts.sort(Comparator.comparing(ContactGeneral::getType));
-//    }
-//
-//    private void sortByAge(List<ContactGeneral> contacts) {
-//        contacts.sort(Comparator.comparing(ContactGeneral::getName));
-//    }
-//
-//    private void sortByNumber(List<ContactGeneral> contacts) {
-//        contacts.sort(Comparator.comparing(ContactGeneral::getNumber));
-//    }
-//
-//    private void sortByDob(List<ContactGeneral> contacts) {
-//        contacts.sort(Comparator.comparing(ContactGeneral::getDob));
-//    }
 
--- workspace/JavaWork/practiceJava/src/main/java/kosta/mission2/mission2_07/domain/command/SystemCommand.java
+++ workspace/JavaWork/practiceJava/src/main/java/kosta/mission2/mission2_07/domain/command/SystemCommand.java
@@ -2,50 +2,64 @@ package kosta.mission2.mission2_07.domain.command;
 
 import kosta.mission2.mission2_07.domain.Controller;
 import kosta.mission2.mission2_07.domain.Management;
+import kosta.mission2.mission2_07.domain.Result;
 
 import java.util.Arrays;
 
-public enum SystemCommand implements Command{
+public enum SystemCommand implements Command {
 
     ADD_INFO(1) {
         @Override
-        public void handleCommand(Management management) {
-            management.addInfo();
+        public Result handleCommand(Management management) {
+            return management.addInfo();
         }
     },
     PRINT_INFO_ALL(2) {
         @Override
-        public void handleCommand(Management management) {
-            management.printInfoAll();
+        public Result handleCommand(Management management) {
+            return management.printInfoAll();
         }
     },
     PRINT_INFO_BY_NAME(3) {
         @Override
-        public void handleCommand(Management management) {
-            management.printInfoByName();
+        public Result handleCommand(Management management) {
+            return management.printInfoByName();
         }
     },
     UPDATE(4) {
         @Override
-        public void handleCommand(Management management) {
-            management.update();
+        public Result handleCommand(Management management) {
+            return management.update();
         }
     },
     DELETE(5) {
         @Override
-        public void handleCommand(Management management) {
-            management.delete();
+        public Result handleCommand(Management management) {
+            return management.delete();
         }
     },
     SORT(6) {
         @Override
-        public void handleCommand(Management management) {
-            management.handleSortRequest(management);
+        public Result handleCommand(Management management) {
+            return management.handleSortRequest(management);
+        }
+    },
+    SAVE_FILE(7) {
+        @Override
+        public Result handleCommand(Management management) {
+            return management.saveFile();
+        }
+    },
+    LOAD_FILE(8) {
+        @Override
+        public Result handleCommand(Management management) {
+            return management.loadFile();
         }
     },
     EXIT(Controller.QUIT_COMMAND) {
         @Override
-        public void handleCommand(Management management) {
+        public Result handleCommand(Management management) {
+            return management.quit();
         }
     };
 
