8b160821a749656abfc21fbf5bc38a74e2bd9087
renechoi <renechoi90@gmail.com>
2023-01-31 23:22:49 +0900

    feat(day28): clear not use

--- workspace/JavaWork/practiceJava/web/frontpage/register.js
+++
@@ -1,9 +0,0 @@
-$("body").prepend("<header></header>");
-$("header").load("./header.html header");
-
-
-function clickthis(){
-    console.log("click1!!!!");
-    location.href='login-page.html'
-
-}
